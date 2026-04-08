6539e5b4dda5f0b1de3c60057c1a5b06c9bceb83
Luphuc2005 <phucga150625@gmail.com>
2026-04-08 13:15:49 +0700

    tăng dropout, lr, agumentation

--- src/data/transform.py
+++ src/data/transform.py
@@ -5,13 +5,15 @@ def build_transforms():
     - Train: lật xoay, ảnh ... (Data Augmentation) để mô hình học tổng quát hơn.
     - Val/Test: chuan hoa size
     """
+   # Tăng cường dữ liệu trong dataloader.py
     train_transform = transforms.Compose([
-        transforms.Resize((256, 256)),
-        transforms.RandomCrop((224, 224)),
-        transforms.RandomHorizontalFlip(), # Lật ngang ngẫu nhiên
+        transforms.RandomResizedCrop(224, scale=(0.8, 1.0)), # Cắt ngẫu nhiên
+        transforms.RandomHorizontalFlip(),                   # Lật ảnh ngang
+        transforms.ColorJitter(brightness=0.2, contrast=0.2), # Đổi màu nhẹ
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), # Trị số chuẩn của ResNet
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
+
 
     val_test_transform = transforms.Compose([
         transforms.Resize((224, 224)), # Ép chuẩn đúng kích thước thay vì Crop ngẫu nhiên
