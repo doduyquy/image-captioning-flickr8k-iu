813a999f50f69e2a87d4d02995137df395ce384a
Luphuc2005 <phucga150625@gmail.com>
2026-04-08 18:12:51 +0700

    Fusion CBAM

--- src/models/CNN/cnn_encoder_fusion.py
+++ src/models/CNN/cnn_encoder_fusion.py
@@ -2,6 +2,51 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
+
+class ChannelAttention(nn.Module):
+    def __init__(self, in_channels, reduction=16):
+        super().__init__()
+        hidden = max(in_channels // reduction, 1)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.mlp = nn.Sequential(
+            nn.Conv2d(in_channels, hidden, kernel_size=1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(hidden, in_channels, kernel_size=1, bias=False)
+        )
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.mlp(self.avg_pool(x)) # hiểu tổng thể
+        max_out = self.mlp(self.max_pool(x)) # hiểu điểm nổi bật
+        attn = self.sigmoid(avg_out + max_out) # cộng lại
+        return x * attn # nhân vào để điều chỉnh độ quan trọng của model
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel=7):
+        super().__init__()
+        assert kernel in (3, 7)
+        padding = 3 if kernel == 7 else 1
+        self.conv1=nn.Conv2d(2, 1, kernel_size=kernel, padding=padding, bias=False)
+        self.sigmoid=nn.Sigmoid()
+    def forward(self,x):
+        avg_out=torch.mean(x,dim=1,keepdim=True) # tinhs trng bình của tất feature tại vị trí [i,j] của 512 chiều
+        max_out=torch.max(x,dim=1,keepdim=True)[0] # tính max của tất feature tại vị trí [i,j] của 512 chiều
+        attn = torch.cat([avg_out, max_out], dim=1)
+        attn = self.sigmoid(self.conv1(attn))# ghép lại thành 2 kênh, sau đó đưa về conv1 rồi lấy sigmoid để tính xác xuất
+        return x*attn #ouput[B,512,7,7], chỗ này tạo 512d cái ma trận của spatial 
+class CBAM(nn.Module):
+    def __init__(self, in_channels, reduction=16, kernel_size=7):
+        super().__init__()
+        self.channel_attention = ChannelAttention(in_channels, reduction)
+        self.spatial_attention = SpatialAttention(kernel_size)
+    def forward(self, x):
+        x = self.channel_attention(x)
+        x = self.spatial_attention(x)
+        return x
+
+
+
 class CNNEncoderFusion(nn.Module):
     def __init__(self, embed_dim=512, refine=True):
         super().__init__()
@@ -31,12 +76,14 @@ class CNNEncoderFusion(nn.Module):
             nn.BatchNorm2d(embed_dim),
             nn.SiLU(inplace=True)
         )
+        self.cbam=CBAM(in_channels=512)
 
     def forward(self, images):
         x=self.stage0_5(images) # [14x14x112]
         f6=self.stage6(x) # [7x7x192]
         f7=self.stage7(f6) # [7x7x320]
         fusion=torch.cat([f6,f7], dim=1) # [7x7x512]
+        fusion=self.cbam(fusion)
         if self.refine:
             fusion=self.fuse_conv(fusion)
         B,C,H,W=fusion.shape
--- src/models/transformer.py
+++ src/models/transformer.py
@@ -19,8 +19,8 @@ class TransformerCaptionModel(BaseCaptionModel):
         max_len = config['model'].get('max_len', 100) # Flickr8k thường max 40-50, 100 là an toàn
         
         # 1. Trích xuất đặc trưng hạ tầng (CNN)
-        self.cnn_encoder = CNNEncoder(embed_dim=embed_dim)
-        # self.cnn_encoder_fusion = CNNEncoderFusion(embed_dim=embed_dim)
+        # self.cnn_encoder = CNNEncoder(embed_dim=embed_dim)
+        self.cnn_encoder_fusion = CNNEncoderFusion(embed_dim=embed_dim)
         # 2. Xử lý không gian bằng Transformer Encoder
         self.spatial_encoder = TransformerEncoder(
             embed_dim=embed_dim, 
@@ -42,7 +42,7 @@ class TransformerCaptionModel(BaseCaptionModel):
         captions: [B, T]
         """
         # CNN trích xuất đặc trưng: [B, 49, 512]
-        features = self.cnn_encoder(images)
+        features = self.cnn_encoder_fusion(images)
         
         # Spatial Transformer Encoder tinh chỉnh đặc trưng: [B, 49, 512]
         features = self.spatial_encoder(features)
@@ -77,7 +77,7 @@ class TransformerCaptionModel(BaseCaptionModel):
         images = images.to(device)
         
         # 1. Encoding
-        features = self.spatial_encoder(self.cnn_encoder(images))
+        features = self.spatial_encoder(self.cnn_encoder_fusion(images))
         
         # 2. Decoding (Greedy)
         start_token = vocab.stoi["<start>"]
@@ -103,7 +103,7 @@ class TransformerCaptionModel(BaseCaptionModel):
         end_token = vocab.stoi["<end>"]
         
         # 1. Encoding
-        features = self.spatial_encoder(self.cnn_encoder(images)) # [B, 49, 512]
+        features = self.spatial_encoder(self.cnn_encoder_fusion(images)) # [B, 49, 512]
         
         # 2. Khởi tạo Beam
         # Một beam item: (chuỗi các token, điểm xác suất tích lũy)
